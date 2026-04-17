d9b66f5afaa212bc64b26280d68fe87765677dbb
haloua5 <wangzikang20010412@163.com>
2026-04-17 19:45:37 +0800

    handle first update

---
+++ include/handle.h
@@ -0,0 +1,17 @@
+#ifndef _HANDLE_H_
+#define _HANDLE_H_
+#include<my_header.h>
+#define SERVER_BASE_DIR "./upload"  //磁盘根目录
+void send_response(int client_fd, const char *msg);// 发送到客户端
+
+void handle_request(int listen_fd);  //相当于worker 判断逻辑都在里面，可以直接调用
+
+void handle_cd(int listen_fd,char *current_path,char *arg);
+void handle_ls(int listen_fd,char *current_path);
+void handle_pwd(int listen_fd,char *current_path);
+void handle_rm(int listen_fd,char *current_path,char *arg);
+void handle_mkdir(int listen_fd,char *current_path,char *arg);
+void handle_gets(int listen_fd, char *current_path, char *arg);
+void handle_puts(int listen_fd, char *current_path, char *arg);
+#endif /* _\=filename_H_ */
+
